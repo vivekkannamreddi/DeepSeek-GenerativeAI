133800059264a58b95c8780a1256982196358423
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-03-29 18:31:31 +0530

    update1

--- deepseek/components/Messages.jsx
+++ deepseek/components/Messages.jsx
@@ -4,9 +4,13 @@ import React from 'react'
 
 const Messages = ({role,content}) => {
   return (
+
+
     <div className='flex flex-col items-center w-full max-w-3xl text-sm '>
         <div className={`flex flex-col w-full mb-8 ${role==='user'&&'items-end'}`}>
             <div className={`group relative flex max-w-2xl py-3 rounded-xl ${role==='user'?'bg-[#414158] px-5':'gap-3'} `}>
+
+
                 <div className={`opacity-0 group-hover:opacity-100 absolute ${role==='user'?'-left-16 top-2.5':'left-9 -bottom-6'} transition-all `}>
                     <div className='flex items-center opacity-70 gap-2'>
                         {
