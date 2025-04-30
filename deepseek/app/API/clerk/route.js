e45f85803230f580a8ac23b30010809675501bae
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-04-30 19:03:48 +0530

    new commit

--- deepseek/app/API/clerk/route.js
+++ deepseek/app/API/clerk/route.js
@@ -3,6 +3,7 @@ import  {webhook} from Svix;
 import connectdb from "@/config/db";
 import User from "@/models/User";
 import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
 export async function POST(req){
     const wh = new webhook(process.env.SIGNING_SECRET);
@@ -30,4 +31,22 @@ export async function POST(req){
 
 
     await connectdb();
+
+    switch(type){
+        case 'user.created':
+            await User.create(userdata)
+            break;
+
+        case 'user.updated':
+            await User.findByIdAndUpdate(data.id,userdata)
+            break;
+
+        case 'user.deleted':
+            await User.findByIdAndDelete(data.id)
+            break;
+
+        default:
+            break;
+    }
+    return NextRequest.json({message:"event received"})
 }
