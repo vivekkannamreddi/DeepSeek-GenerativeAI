2ac743633a02e0b47ef36716a0c766adbb7c326d
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-05-03 19:31:59 +0530

    updated the import statements

--- deepseek/app/API/chat/create/route.js
+++ deepseek/app/API/chat/create/route.js
@@ -1,6 +1,6 @@
 import connectdb from "@/config/db";
 import Chat from "@/models/Chat";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(req){
